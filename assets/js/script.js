745e77991f7d852b9ff43ca2d3b6e3ae75f12f69
dev_vitor <dev_vitor912@hotmail.com>
2023-07-25 22:47:10 -0300

    Add forEach - JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado');
+        
+        personagem.classList.add('selecionado'); 
+    })    
+});
