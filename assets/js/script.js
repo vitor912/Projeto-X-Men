df5b83fbbe063f6686ab929d07f80240c032084a
dev_vitor <dev_vitor912@hotmail.com>
2023-07-27 22:41:45 -0300

    Criando novas Function em JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,30 @@ const personagens = document.querySelectorAll('.personagem');
 
 personagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter', () => {
+
+        // If = Somente para celular 
+        if(window.innerWidth < 450) {
+            window.scrollTo({
+                top: 0, behavior: 'smooth'
+            });
+        }
+
+
         const personagemSelecionado = document.querySelector('.selecionado');
         personagemSelecionado.classList.remove('selecionado');
         
-        personagem.classList.add('selecionado'); 
+        personagem.classList.add('selecionado');
+
+
+        const personagemGrande = document.querySelector('.personagemGrande');
+        const idPersonagem = personagem.attributes.id.value;        
+        personagemGrande.src = `./assets/img/card-${idPersonagem}.png`;
+
+
+        const nomePersonagens = document.getElementById('nomePersonagem');        
+        nomePersonagens.innerText = personagem.getAttribute('data-name');
+
+        const desPersonagens = document.getElementById('descricaoPersonagem');
+        desPersonagens.innerText = personagem.getAttribute('data-description');          
     })    
 });
